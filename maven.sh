7f500264a14e4382cfa29af81b19279629493546
prudhvisurya123 <prudhvisurya.devops2019@gmail.com>
2025-11-11 19:07:36 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 echo "=== Building Java Project ==="
 mvn clean verify
+set -e
+
+echo "=== Building and Analyzing Java Project with SonarQube ==="
+
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=Test-Project \
+  -Dsonar.projectName='Test-Project' \
+  -Dsonar.host.url=http://51.20.185.44:9001 \
+  -Dsonar.token=squ_69167e9fd610e5cbeea18493cbaeb438ee0c52e6 \
+  -Dsonar.sourceEncoding=UTF-8 \
+  -Dsonar.qualitygate.wait=true
 
 
