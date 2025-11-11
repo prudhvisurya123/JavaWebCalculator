c15acde37b6802e531b9e498f5d6367398a286c7
prudhvisurya123 <prudhvisurya.devops2019@gmail.com>
2025-11-11 18:44:37 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
 
+# === SonarQube Configuration ===
+SONAR_HOST_URL=${SONAR_HOST_URL:-"http://51.20.185.44:9001"}
+SONAR_TOKEN=${SONAR_TOKEN:-"squ_69167e9fd610e5cbeea18493cbaeb438ee0c52e6"}
+
 echo "=== Building Java Project ==="
 mvn clean verify
 
 echo "=== Running SonarQube Analysis ==="
-mvn sonar:sonar \
+mvn org.sonarsource.scanner.maven:sonar-maven-plugin:4.0.0.4121:sonar \
   -Dsonar.projectKey=JavaWebCalculator \
   -Dsonar.host.url=$SONAR_HOST_URL \
   -Dsonar.login=$SONAR_TOKEN \
