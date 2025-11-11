5db7c0b845e17ad49e55816c37e97e64f5af7c03
prudhvisurya123 <prudhvisurya.devops2019@gmail.com>
2025-11-11 18:41:36 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-echo "Building Java project..."
-mvn clean package
+set -e
+
+echo "=== Building Java Project ==="
+mvn clean verify
+
+echo "=== Running SonarQube Analysis ==="
+mvn sonar:sonar \
+  -Dsonar.projectKey=JavaWebCalculator \
+  -Dsonar.host.url=$SONAR_HOST_URL \
+  -Dsonar.login=$SONAR_TOKEN \
+  -Dsonar.sourceEncoding=UTF-8 \
+  -Dsonar.qualitygate.wait=true
